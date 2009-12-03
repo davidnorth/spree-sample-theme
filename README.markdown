d59a297a165c7cecac6917b51e70e480c932e6bb
David North <davidnorth@gmail.com>
2009-12-03 13:33:07 +0000

    initial commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+= Sample Theme
+
+Description goes here
---
+++ sample_theme_extension_hooks.rb
@@ -0,0 +1,5 @@
+class SampleThemeExtensionHooks < Spree::ThemeSupport::Hook::ViewListener
+
+  # render_on :homepage_above_products, :partial => "shared/my_partial" 
+
+end
